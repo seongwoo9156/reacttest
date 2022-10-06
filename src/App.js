9f7e8dc1713ae399bd15944159fab191976432e3
seongwoo <seongwoo91@naver.com>
2022-10-06 10:36:28 +0900

    시험커밋

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [list, setlist] = useState(["신나는시험시간", "리액트열심히배우기"]);
+  const [input, setinput] = useState("");
+
+  const inputChange = (e) => {
+    setinput(e.target.value);
+  };
+  console.log(input);
+
+  const addlist = () => {
+    setlist((list) => {
+      return [input, ...list];
+    });
+  };
+
+  return (
+    <div className="contatiner">
+      <div className="inputBox">
+        <input type="text" value={input} onChange={inputChange} />
+        <button onClick={addlist}>추가하기</button>
+      </div>
+      <h1>Todo List</h1>
+      <div className="list">
+        <ul>
+          {list.map((e, i) => {
+            return <li key={i}>{e}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
